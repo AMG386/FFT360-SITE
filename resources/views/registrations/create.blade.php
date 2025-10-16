aa8e3812f30fd0a58cc4b05a97d3516ad712ed7b
Akshay M G <akshaymg46@gmail.com>
2025-10-16 10:45:51 +0530

    chang in spelling

--- resources/views/registrations/create.blade.php
+++ resources/views/registrations/create.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-  <title>FFT360 • Client Intake Form</title>
+  <title>FFT360 </title>
   <script src="https://cdn.tailwindcss.com"></script>
   <style>
     :root{
