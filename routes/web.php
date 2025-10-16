2841f4db2d98badda20bd2ec41a003a64693e540
Akshay M G <akshaymg46@gmail.com>
2025-10-16 10:29:05 +0530

    issues cleared

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegistrationController;
 
 Route::get('/', function () {
     return view('index');
@@ -10,6 +11,7 @@ Route::view('/about-us', 'about-us')->name('about-us');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/gallery', 'gallery')->name('gallery');
 Route::view('/blog_details', 'blog_details')->name('blog_details');
+Route::view('/client_intake', 'client_intake')->name('client_intake');
 Route::view('/blog', 'blog')->name('blog');
 Route::view('/bmi_calculator', 'bmi_calculator')->name('bmi_calculator');
 Route::view('/class_details', 'class_details')->name('class_details');
@@ -18,3 +20,5 @@ Route::view('/services', 'services')->name('services');
 Route::view('/team', 'team')->name('team');
 Route::view('/404', '404')->name('404');
 
+Route::post('/registrations/store', [RegistrationController::class, 'store'])->name('registrations.store');
+Route::get('/registrations/create', [RegistrationController::class, 'create'])->name('registrations.create');
